c62c01231fb626b411119cab8ccda5b9d918f4d0
frojak4 <Frojak4@gmail.com>
2025-01-14 16:55:01 +0100

    fixed animations

--- components/Exercise.tsx
+++ components/Exercise.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { aleo } from '@/fonts'
-import { ExerciseType } from '@/lib/types'
+import { ExerciseType, Step } from '@/lib/types'
 import React, { useEffect, useState } from 'react'
 import { Button } from './ui/button'
 import * as motion from "motion/react-client"
@@ -13,18 +13,35 @@ const Exercise = ({ data }: { data: ExerciseType }) => {
 
     const [started, setStarted] = useState(false)
     const [finished, setFinished] = useState(false)
-    const [step, setStep] = useState(1)
+    const [step, setStep] = useState(0)
     const [length, setLength] = useState(16)
     const [text, setText] = useState<string>(data.steps[0].text)
+    const [scale, setScale] = useState<number>(1)
 
+    console.log(data.color)
+
+    const handleBreathing = (step: Step) => {
+        if (step.type === 'exhale') {
+            setScale(1)
+        } else if (step.type === 'inhale') {
+            setScale(2)
+        }
+        setText(step.text)
+        setStep((prev) => prev + 1)
+    }
+
+    const handleStart = () => {
+        handleBreathing(data.steps[0])
+        setStarted(true)
+    }
 
     useEffect(() => {
         if (started && !finished) {
+            console.log('test')
             const interval = setTimeout(() => {
                 if (length > step) {
-                    console.log(data.steps[step % data.steps.length].text)
-                    setText(data.steps[step % data.steps.length].text)
-                    setStep((prev) => prev + 1)
+                    console.log('frode')
+                    handleBreathing(data.steps[step % data.steps.length])
                 } else {
                     setFinished(true)
                     console.log('All done with your exercise')
@@ -37,14 +54,13 @@ const Exercise = ({ data }: { data: ExerciseType }) => {
             return () => clearTimeout(interval)
         }
 
-
-
     }, [started, step])
 
     return (
-        <div className="bg-secondary rounded-2xl mx-auto w-4/5 md:w-2/5 mt-6 p-6">
+        <>
+            {!started ? <div className="bg-secondary rounded-2xl mx-auto w-4/5 md:w-2/5 mt-6 p-6">
 
-            {!started ?
+
                 <div className="flex-col justify-center items-center text-center">
                     <span className="text-center">
                         <h1 className={`${aleo.variable} text-3xl md:text-4xl font-aleo py-2`}>{data.name}</h1>
@@ -55,18 +71,24 @@ const Exercise = ({ data }: { data: ExerciseType }) => {
                         })}
                     </ul>
 
-                    <Button onClick={() => setStarted(true)}>Start</Button>
+                    <Button onClick={handleStart}>Start</Button>
                 </div>
+            </div >
                 :
-                <motion.div
-                    initial={{ opacity: 0, scale: 0 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    transition={{
-                        duration: 0.4,
-                        scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+                <motion.div className={`border-2 border-${data.color}-400 text-center flex items-center mt-32 mx-auto shadow-2xl dark:shadow-gray-200/20`}
+                    style={{
+                        width: 150,
+                        height: 150,
+                        borderRadius: "50%",
+
+
                     }}
-                >Hallo</motion.div>}
-        </div>
+                    animate={{ scale }} // Animate scale based on state
+                    transition={{ duration: 4, ease: "easeInOut" }} // Smooth easing 
+                >
+                    <h3 className="text-sm">{text}</h3> </motion.div >}
+
+        </>
     )
 }
 
